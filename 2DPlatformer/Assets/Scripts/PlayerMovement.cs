42eee572cd2e036d7dd53556e8cc77e7704910c5
Matheus Tessaro <woeismatt_@hotmail.com>
2025-06-15 11:35:34 -0300

    Attributed layer values to walls and floors and added wall jumping mechanics

--- 2DPlatformer/Assets/Scripts/PlayerMovement.cs
+++ 2DPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,29 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Creates a reference to the rigidbody for the player character
+    // Creates a references to game object's components
     private Rigidbody2D rb2d;
-
-    // Creates a reference to the Animator for the player character
     private Animator animator;
+    private BoxCollider2D boxCollider;
+
+    private float horizontalInput;
+
+    private float wallJumpCooldown;
 
     // This annotation allows to change the value directly from unity
     [SerializeField]
     private float speed;
 
-    private bool grounded;
+    [SerializeField]
+    private float jumpPower;
+
+    [SerializeField]
+    private LayerMask groundLayerMask;
+
+    [SerializeField]
+    private LayerMask wallLayerMask;
 
     // Awake is called every time the script is loaded
     private void Awake()
@@ -20,25 +31,13 @@ public class PlayerMovement : MonoBehaviour
         // Grab references from game object
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    //void Start()
-    //{
-
-    //}
 
     // Update is called once per frame
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        // linearVelocity = Controls the velocity of the player's movement
-        // Vector2 = Controls the position, it's directly linked to the "Transform" option.
-        //       x = Up/Down
-        //       y = Left/Right
-        //       z = Backwards/Fowards (not present in 2D)
-        rb2d.linearVelocity = new Vector2(horizontalInput * speed, rb2d.linearVelocityY);
+        horizontalInput = Input.GetAxis("Horizontal");
 
         // Flips character sprite based on the X axis value
         // Negative = Character is moving left
@@ -48,25 +47,80 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector2(-1, 1);
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
-            Jump();
-
         // Set animator parameters
         animator.SetBool("run", horizontalInput != 0);  // horizontalInput will be 0 if no inputs are being triggered (idle)
-        animator.SetBool("grounded", grounded);
+        animator.SetBool("grounded", IsGrounded());
+
+        if (wallJumpCooldown > 0.2f)
+        {
+            // linearVelocity = Controls the velocity of the player's movement
+            // Vector2 = Controls the position, it's directly linked to the "Transform" option.
+            //       x = Up/Down
+            //       y = Left/Right
+            //       z = Backwards/Fowards (not present in 2D)
+            rb2d.linearVelocity = new Vector2(horizontalInput * speed, rb2d.linearVelocityY);
+
+            if (OnWall() && !IsGrounded())
+            {
+                rb2d.gravityScale = 0;
+                rb2d.linearVelocity = Vector2.zero;
+            }
+            else
+                rb2d.gravityScale = 2f;
+
+            if (Input.GetKey(KeyCode.Space))
+                Jump();
+        }
+        else
+            wallJumpCooldown += Time.deltaTime;
     }
 
     private void Jump()
     {
-        animator.SetTrigger("jump");
-        grounded = false;
-        rb2d.linearVelocity = new Vector2(rb2d.linearVelocityX, speed);    // Maintain the X axis speed, change the value on the Y axis
+        if (IsGrounded())
+        {
+            animator.SetTrigger("jump");
+            rb2d.linearVelocity = new Vector2(rb2d.linearVelocityX, jumpPower);             // Maintain the X axis speed, change the value on the Y axis
+        }
+        else if (OnWall() && !IsGrounded())
+        {
+            if (horizontalInput == 0)
+            {
+                rb2d.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0f);
+            }
+            else
+                rb2d.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6f);  // Pushes player against the wall for wall jumping mechanics
+
+            wallJumpCooldown = 0f;
+        }
     }
 
     // Detects collisions between 2D objects
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
     }
+
+    private bool IsGrounded()
+    {
+        // Casts a ray from a point of origin,
+        // if the line intersects with an object with a collider then returns "true".
+        return !Physics2D.BoxCast(
+            boxCollider.bounds.center,
+            boxCollider.bounds.size,
+            0,
+            Vector2.down,
+            0.1f,
+            groundLayerMask
+        ).collider.IsUnityNull();
+    }
+
+    private bool OnWall() =>
+        !Physics2D.BoxCast(
+            boxCollider.bounds.center,
+            boxCollider.bounds.size,
+            0,
+            new(transform.localScale.x, 0),
+            0.1f,
+            wallLayerMask
+        ).collider.IsUnityNull();
 }
